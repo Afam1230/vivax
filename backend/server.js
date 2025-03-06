c47a13a7ce4a3a9cdd55da0ef08eedb178de8c8a
Afam1230 <jagannathonwuegbuzie@gmail.com>
2025-03-06 10:36:36 +0100

    jdh

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const path = require('path')
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const sendEmail = require('./sendEmail');
-
+const port = process.env.PORT || 5000;
 
 
 
@@ -310,5 +310,4 @@ const AccountRequestSchema = new mongoose.Schema({
 
 
 // Start Server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on  localhost:${PORT}`));
+app.listen(port, () => console.log(`Server running on  localhost:${PORT}`));
