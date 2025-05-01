2736b2411f1d669f320f9d007a1ca49679e90182
Afam1230 <jagannathonwuegbuzie@gmail.com>
2025-05-01 04:07:24 +0100

    zz

--- backend/server.js
+++ backend/server.js
@@ -50,12 +50,12 @@ app.get('/api/news', async (req, res) => {
 
   
 
-// if (process.env.NODE_ENV === "production") {
-// 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-// 	app.get("*", (req, res) => {
-// 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-// 	});
-// }
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 app.listen(PORT, () => {
 	connectDB();
